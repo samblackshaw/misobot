30fa144af6f9a8e9831f515283511c9496cdff89
Niki Liu <nishiki.liu@gmail.com>
2015-06-17 20:34:30 -0400

    Add more comments in twitch_helper.rb

--- app/helpers/twitch_helper.rb
+++ app/helpers/twitch_helper.rb
@@ -1,14 +1,18 @@
 #-------------------------------------------------------------------------------
 # twitch_helper.rb
 #
+# Helpers that assist in making calls to the psuedo IRC platform Twitch has.
+#-------------------------------------------------------------------------------
+
 # Altered implementation of a reply method that circumvents the limitations of
 # Twitch IRC. Reference: https://github.com/cinchrb/cinch/issues/151
+# @param {string} - message
 #-------------------------------------------------------------------------------
 class Cinch::Message
-  def twitch(string)
-    string = string.to_s.gsub("<","&lt;").gsub(">","&gt;")
+  def twitch(message)
+    message = message.to_s.gsub("<","&lt;").gsub(">","&gt;")
     bot.irc.send ":#{bot.config.user}!#{bot.config.user}" +
                  "@#{bot.config.user}.tmi.twitch.tv PRIVMSG " +
-                 "##{ENV['TWITCH_USER']} :#{string}"
+                 "##{ENV['TWITCH_USER']} :#{message}"
   end
 end
