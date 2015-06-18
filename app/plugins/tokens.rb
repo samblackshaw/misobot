40383b9730c5fc5492944ceb67c3cf8f597d1441
Niki Liu <nishiki.liu@gmail.com>
2015-06-17 20:50:26 -0400

    Move helper token methods into Tokens class

--- app/plugins/tokens.rb
+++ app/plugins/tokens.rb
@@ -9,7 +9,8 @@
 # - ActiveRecord
 #===============================================================================
 
-# Base Tokens class to kickstart automated processes and store persistent data.
+# Base Tokens class to store tokens-specific data and methods that can be used
+# in all Tokens-related plugins.
 class Tokens
   NAME         = ENV["TOKENS_NAME"]
   REFRESH_TIME = 15.minutes
@@ -30,6 +31,21 @@ class Tokens
         end
       end
     end
+  end
+
+  # Removes {amount} from {username}'s token value. Value cannot dip below 0.
+  # @param {string}  - username
+  # @param {integer} - amount
+  # @param {message} - m
+  def self.remove(username, amount, m)
+    username.gsub!("@", "")
+    user = User.find_or_create_by(name: username)
+
+    # Deduct amount from number of tokens
+    tokens = user.tokens - amount; tokens = 0 if tokens < 0
+    user.update_attributes(tokens: tokens)
+    m.twitch "SMOrc @#{user.name}, you have been penalized #{amount} " +
+             "#{Tokens::NAME}."
   end
 end
 
@@ -66,25 +82,10 @@ class Tokens::PenalizeTokens
   def execute(m)
     if MisoSTM.is_mod? m.user.nick
       params = m.params[-1].split(" ")
-      penalize(params[1], PENALIZE_BY, m) if params.count == 2
+      Tokens.remove(params[1], PENALIZE_BY, m) if params.count == 2
     else
       m.twitch "SwiftRage @#{m.user.nick}, you ain't no mod!"
-      penalize(m.user.nick, SOFT_PENALTY, m)
+      Tokens.remove(m.user.nick, SOFT_PENALTY, m)
     end
-  end
-
-  # Removes {amount} from {username}'s token value. Value cannot dip below 0.
-  # @param {string}  - username
-  # @param {integer} - amount
-  # @param {message} - m
-  def penalize(username, amount, m)
-    username.gsub!("@", "")
-    user = User.find_or_create_by(name: username)
-
-    # Deduct amount from number of tokens
-    tokens = user.tokens - amount; tokens = 0 if tokens < 0
-    user.update_attributes(tokens: tokens)
-    m.twitch "SMOrc @#{user.name}, you have been penalized #{amount} " +
-             "#{Tokens::NAME}."
   end
 end
